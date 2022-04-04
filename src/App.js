f322f730d7739d37e7ea790a69628d054954574e
Mazen <mazenadel19@gmail.com>
2022-04-04 23:43:47 +0200

    Added Colors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const oneMin = 60_000
+const colors = ["#46BCDE", "#52D273", "#E94F64", "#E57254", "#e5c453", '#40e0d0','#673ab7','#9c27b0']
+
 export default class App extends Component {
 	state = {
 		time: new Date().toLocaleTimeString(),
@@ -10,6 +13,10 @@ export default class App extends Component {
 		setInterval(() => {
 			this.setState({ time: new Date().toLocaleTimeString() });
 		}, 1000);
+
+		setInterval(()=>{
+			document.querySelector(".timeStyle").style.color=colors[Math.round(Math.random()*7)];
+		},oneMin*5)
 	}
 
 
